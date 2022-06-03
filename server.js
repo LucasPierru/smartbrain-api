54b96a12d787b52c3bfe85d8449c3e89f5d5247a
LucasPierru <lucaspierru7@gmail.com>
2022-06-03 14:56:47 -0400

    database deployement

--- server.js
+++ server.js
@@ -10,11 +10,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host : 'postgresql-amorphous-63807',
-        //port : 5432,
-        user : 'postgres',
-        password : '',
-        database : 'smartbrain'
+        host : process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
